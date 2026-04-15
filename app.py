98adf9d6bc2f77d55ddca9ecc55888c04f584525
Pranay Singh <TheKush27@gmail.com>
2026-04-15 11:07:02 +0530

    show strategy result on UI

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
-from fastapi import FastAPI, Request
-from fastapi.templating import Jinja2Templates
+from fastapi import FastAPI, Form
+from fastapi.responses import HTMLResponse
+from engines.strategy_engine import generate_strategy
 
 app = FastAPI()
 
-templates = Jinja2Templates(directory="templates")
+@app.get("/", response_class=HTMLResponse)
+def home():
+    return """
+    <h1>DropMeOnline</h1>
+    <form action="/generate-strategy" method="post">
+        <input type="text" name="goal" placeholder="Enter your goal">
+        <button type="submit">Generate</button>
+    </form>
+    """
 
-@app.get("/")
-def home(request: Request):
-    return templates.TemplateResponse(
-        "index.html",
-        {"request": request}
-    )
+@app.post("/generate-strategy", response_class=HTMLResponse)
+def generate(goal: str = Form(...)):
+    result = generate_strategy(goal)
+
+    return f"""
+    <h1>Strategy Generated</h1>
+    <p>{result['goal']}</p>
+    <a href="/">Back</a>
+    """
